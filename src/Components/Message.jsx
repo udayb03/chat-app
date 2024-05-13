82d9391f29e2865b7a983569d3a727a5ed3702c4
Uday Baldha <udaybaldha310@gmail.com>
2024-05-13 15:47:05 +0530

    first commit

---
+++ src/Components/Message.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {HStack,Avatar,Text} from '@chakra-ui/react'
+
+const Message = ({text,uri,user='other'}) => {
+  return (
+    <HStack
+      alignSelf={user === "me" ? "flex-end" : "flex-start"}
+      borderRadius={"base"}
+      backgroundColor={"gray.100"}
+      paddingY={"2"}
+      paddingX={user === 'me' ?'4' : '2'} >
+    {user === "other" && <Avatar src={uri}></Avatar>}
+    <Text>{text} </Text>
+    {user === "me" && <Avatar src={uri}></Avatar>}
+    </HStack>
+  );
+}
+export default Message;
---
+++ src/Components/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyABMS_1knc-jv2ae4ev9DZg1q9DIgcLKLY",
+  authDomain: "chat-app-6cc99.firebaseapp.com",
+  projectId: "chat-app-6cc99",
+  storageBucket: "chat-app-6cc99.appspot.com",
+  messagingSenderId: "499371019593",
+  appId: "1:499371019593:web:54301862dd02d46f825e98"
+};
+
+export const app = initializeApp(firebaseConfig);
